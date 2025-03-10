e418c891a8349d6278b73658e98b7a747ced87cf
Emil <emil.lasocha9@gmail.com>
2025-03-10 23:27:57 +0100

    Change the implementation, so that it matches the project description

--- ccd/datasets/synthetic.py
+++ ccd/datasets/synthetic.py
@@ -5,50 +5,34 @@ Generation of synthetic datasets.
 import numpy as np
 
 
-def generate_y(p, n):
+def generate_synthetic_data(p, n, d, g):
     """
-    Generates a class variable y, which is a sample of size n and probability p,
-    taken from Bernoulli distribution (which is Binomial distribution with 1 number of tries).
-    """
-    return np.random.binomial(n=1, p=p, size=n)
+    Generates the whole dataset by generating n observations. Firstly it generates the sample from
+    binary class variable Y, which is a sample of probability p, taken from Bernoulli distribution,
+    which is Binomial distribution with only 1 number of tries. Then generates the
+    feature vector X, which for X|Y=0 is taken from normal distribution with mean (0,...,0)
+    and covariance matrix specified below, and for X|Y=1 is taken from
+    normal distribution with mean (1, 1/2, ..., 1/d), and the same covariance matrix,
+    which is defined such that in i-th row and j-th column: a[i][j] = g^{|i-j|}.
 
-
-def covariance_matrix(d, g):
-    """
-    As the covariance matrix of both X|Y=0 and X|Y=1 are the same, here we define it as it
-    is intended, therefore we have a[i][j] = g^{|i-j|}.
-    """
-    return np.array([[g ** abs(i - j) for i in range(d)] for j in range(d)])
-
-
-def generate_X(y, n, d, g):
-    """
-    Generates the feature vector X, which for X|Y=0 is taken from Normal distribution with mean (0,...,0)
-    and covariance as intended and for X|Y=1 is taken from Normal distribution with mean (1, 1/2, ..., 1/d),
-    and the same covariance matrix.
+    :param p: probability of '1'
+    :param n: number of observations
+    :param d: number of features
+    :param g: parameter on which the covariance matrix depends
+    :return: the whole dataset
     """
     X = np.zeros((n, d))
+    y = np.zeros(n)
 
     mean_0 = np.zeros(d)
     mean_1 = np.array([1 / i for i in range(1, d+1)])
-
-    cov = covariance_matrix(d, g)
+    cov = np.array([[g ** abs(i - j) for i in range(d)] for j in range(d)])
 
     for i in range(n):
+        y[i] = np.random.binomial(n=1, p=p)
         if y[i] == 0:
             X[i, :] = np.random.multivariate_normal(mean_0, cov)
         if y[i] == 1:
             X[i, :] = np.random.multivariate_normal(mean_1, cov)
 
-    return X
-
-
-def generate_synthetic(p, n, d, g):
-    """
-    Generates the whole dataset, first by generating the y binary class variable,
-    then by creating the data X, following previous assumptions.
-    """
-    y_vector = generate_y(p, n)
-    X_matrix = generate_X(y_vector, n, d, g)
-
-    return X_matrix, y_vector
+    return X, y
