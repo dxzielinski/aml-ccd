e32156ef7ba024655d38035b755e2cbe851b89ed
dxzielinski <dominik.zielinski2002@gmail.com>
2025-03-27 23:48:44 +0100

    format: fix docstrings

--- ccd/datasets/real.py
+++ ccd/datasets/real.py
@@ -22,6 +22,7 @@ np.random.seed(42)
 def load_data(file):
     """
     Loads the file with .arff format into pandas dataframe.
+
     :param file: the .arff file
     :return: pandas dataframe
     """
@@ -34,6 +35,7 @@ def fill_dummy(df, parameter=0.6):
     """
     It creates a list of the permuted (by columns) dataframes, and then it concatenates them,
     such that the number of observations will be sufficient to the project requirements.
+
     :param df: inserted pandas dataframe
     :param parameter: by default 0.6, the project insists on a number bigger than 0.5
     :return: dataframe with necessary dimensions
@@ -50,6 +52,7 @@ def fill_dummy(df, parameter=0.6):
 def missing_values_check(array):
     """
     Checks whether an array contains missing values.
+
     :param array: numpy array
     :return: true if the numpy array is filled only with numbers, as it is needed to be
     """
@@ -60,6 +63,7 @@ def missing_values_check(array):
 def collinearity_check(df):
     """
     Checks whether there is no such two columns, that one is a scalar multiple of another (collinearity).
+
     :param df: pandas dataframe
     :return: true if there is no collinearity between variables
     """
@@ -73,6 +77,7 @@ def get_dataset_1(path="dataset_1.arff"):
     First it loads the first dataset into pandas dataframe, then creates the target variable y.
     Then, it drops the column with target variable from original dataframe and enlarges to meet the
     project requirements.
+
     :return: cleaned input features X and target variable y
     """
     df1 = load_data(path)
@@ -93,6 +98,7 @@ def get_dataset_2(path="dataset_2.arff"):
     As the target variable is 1-2 and not 0-1, we subtract one from each value.
     Then, it drops the column with target variable from original dataframe and
     enlarges to meet the project requirements.
+
     :return: cleaned input features X and target variable y
     """
     df2 = load_data(path)
